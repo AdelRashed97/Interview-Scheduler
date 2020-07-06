3d3d9e643615e3c71ad3c9a88c4c3581791d7c2b
Adel Rashed <adel_rashed@outlook.com>
2020-07-06 16:03:59 -0400

    used classNames to conditionally select classes

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,12 +9,6 @@ export default function Button(props) {
       "button--confirm":props.confirm,
       "button--danger":props.danger
 })
-   // let buttonClass = "button";
-   // if (props.confirm) {
-   //    buttonClass += " button--confirm";
-   //  } else if (props.danger) {
-   //     buttonClass += " button--danger"
-   //  }
   
    return (<button className={buttonClass}
        onClick={props.onClick}
