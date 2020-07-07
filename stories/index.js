0f3ef28bbe8ef112778efa84727b719a45a77c9a
Adel Rashed <adel_rashed@outlook.com>
2020-07-07 19:27:54 -0400

    create story for show component

--- stories/index.js
+++ stories/index.js
@@ -13,8 +13,7 @@ import InterviewerList from "components/InterviewerList"
 import Appointment from "components/Appointment/index"
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
-
-
+import Show from "components/Appointment/Show"
 
 
 
@@ -148,3 +147,4 @@ storiesOf("Button", module)
         .add("Appointment with Time", () => <Appointment time="12pm" />)
         .add("Header", () =>  <Header time ="12pm"></Header>)
         .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+        .add("Show", () => <Show student ="Lydia Miller-Jones" interviewer ={ interviewer} onDelete ={action("onDelete")} onEdit ={action("onEdit")}/>)
