00752aa6b0a7a0e700626559876da4be01d9975f
Adel Rashed <adel_rashed@outlook.com>
2020-07-07 20:21:17 -0400

    added stories for error component

--- stories/index.js
+++ stories/index.js
@@ -16,7 +16,7 @@ import Empty from "components/Appointment/Empty"
 import Show from "components/Appointment/Show"
 import Confirm from "components/Appointment/Confirm"
 import Status from "components/Appointment/Status"
-
+import Error from "components/Appointment/Error"
 
 storiesOf("Button", module)
   .addParameters({
@@ -154,3 +154,6 @@ storiesOf("Button", module)
 
         .add("Status-Saving", () => <Status message="Saving"/>)
         .add("Status-Delete", () => <Status message="Deleting"/>)
+
+        .add("Error Saving",() => <Error message ="Could not save appointment." onClose={action("onClose")}/>)
+        .add("Error Deleting",() => <Error message ="Could not delete appointment." onClose={action("onClose")}/>)
