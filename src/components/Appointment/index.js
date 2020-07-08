8934ca82751b0db2cf06f76a653ec569a0b165cb
Adel Rashed <adel_rashed@outlook.com>
2020-07-08 13:40:08 -0400

    conditionally render show or empty component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,15 @@
 import React from "react"
+import Header from "./Header"
+import Show from "./Show"
+import Empty from "./Empty"
 import "./style.scss" 
+import { action } from "@storybook/addon-actions";
 
-export default function Appointmnet() {
-  return <article className="appointment"></article>
+
+export default function Appointmnet(props) {
+  const {interview,time}=props
+  return (<article className="appointment">
+    <Header time={time}/>
+    {interview ? <Show student ={interview.student} interviewer ={interview.interviewer} /> : <Empty/>}
+  </article>)
 }
