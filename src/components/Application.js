54af1af6ad2aa7d7ed935c87c02688b3f76e231c
Adel Rashed <adel_rashed@outlook.com>
2020-07-08 18:59:50 -0400

    get days from api server

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,26 @@
 import React from "react";
-import  { useState } from "react";
+import  { useState,useEffect } from "react";
+import axios from "axios"
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment"
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 const appointments = [
   {
@@ -68,7 +69,13 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day,setDay] = useState("Monday")
+  const [days,setDays]=useState([]);
+  const [day,setDay] = useState("Monday");
+  useEffect(() => {
+    axios.get("/api/days")
+    .then(res => {console.log(res.data)
+    setDays(res.data)})
+  },[])
   const parsedAppointments = appointments.map(appointment => <Appointment key ={appointment.id} {...appointment}/>);
   parsedAppointments.push(<Appointment key="last" time="5pm" />)
   return (
