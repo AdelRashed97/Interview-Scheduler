6f6ab6363e2d2da925d03b2428dae12d6969830e
Adel Rashed <adel_rashed@outlook.com>
2020-07-09 18:49:08 -0400

    implemented transition and back function to move between modes

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,25 @@
+import {useState,useEffect} from "react"
+
+export default function useVisualMode(initial) {
+  const [mode,setMode]= useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (nextMode) =>{ 
+    setMode(nextMode)
+    setHistory([...history,nextMode])
+  };
+  
+  const back = () => {
+    //setHistory([...history].pop(),()=> setMode(history.slice(-1)[0]))
+    setHistory(history.slice(0,history.length -1))
+     
+  }
+  
+  useEffect(()=>{
+    setMode(history.slice(-1)[0])
+
+  },[history])
+
+
+  return {mode,transition,back}
+}
