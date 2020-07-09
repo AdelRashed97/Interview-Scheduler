d56b158e0569da94f5d949753aa59e19de4f3623
Adel Rashed <adel_rashed@outlook.com>
2020-07-09 14:06:11 -0400

    added interviewers to state and use axios to get interviewers data

--- src/components/Application.js
+++ src/components/Application.js
@@ -11,7 +11,8 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers:{}
   });
 
   const setDay = day => setState({ ...state, day });
@@ -20,9 +21,15 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       Promise.resolve(axios.get("/api/days")),
-      Promise.resolve(axios.get("/api/appointments")) 
+      Promise.resolve(axios.get("/api/appointments")), 
+      Promise.resolve(axios.get("/api/interviewers"))
     ])
-    .then(all => setState(prev => ({...prev,days:all[0].data,appointments:all[1].data})))
+    .then(all => setState(prev => ({...prev,
+      days:all[0].data,
+      appointments:all[1].data,
+      interviewers:all[2].data
+    })
+    ))
   }
   ,[]);
 
