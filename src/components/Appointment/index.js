d577e6594f9871e3a6422855875a471533e13819
Adel Rashed <adel_rashed@outlook.com>
2020-07-10 09:35:37 -0400

    switch between modes using useVisualMode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,13 +3,28 @@ import Header from "./Header"
 import Show from "./Show"
 import Empty from "./Empty"
 import "./style.scss" 
+import useVisualMode from "../../hooks/useVisualMode"
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE ="CREATE"
 
 
 
 export default function Appointmnet(props) {
   const {interview,time}=props
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  
+ 
   return (<article className="appointment">
     <Header time={time}/>
-    {interview ? <Show student ={interview.student} interviewer ={interview.interviewer} /> : <Empty/>}
+    
+
+    {mode === EMPTY && <Empty onAdd={()=>transition(CREATE) }/>}
+
+    {mode === SHOW && <Show student ={interview.student} interviewer ={ interview.interviewer} onDelete ={() => console.log("delete")} onEdit ={() => console.log("edit")}/>}
+
   </article>)
 }
