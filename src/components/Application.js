590fa4576714bbd7d210e53d5818e5d0112e2ac7
Adel Rashed <adel_rashed@outlook.com>
2020-07-10 14:14:40 -0400

    use getInterviwerForDay to pass interviwers to apponitment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import axios from "axios"
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment"
-import { getAppointmentsForDay,getInterview } from "helpers/selectors";
+import { getAppointmentsForDay,getInterview,getInterviewersForDay } from "helpers/selectors";
 
 
 export default function Application(props) {
@@ -34,7 +34,7 @@ export default function Application(props) {
   ,[]);
 
   const appointments = getAppointmentsForDay(state,state.day)
-  
+  const interviewers = getInterviewersForDay(state,state.day)
  
 const schedule = appointments.map((appointment) => {
   const interview = getInterview(state, appointment.interview);
@@ -45,6 +45,7 @@ const schedule = appointments.map((appointment) => {
       id={appointment.id}
       time={appointment.time}
       interview={interview}
+      interviewers={interviewers}
     />
   );
 });
